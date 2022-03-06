933b1f8a14cc5f4e3e5f4747f71b468032dc5103
Max Fletcher <mfletcher@umail.ucsb.edu>
2022-03-06 12:19:57 -0800

    Add bird and controls + Don't draw in fill_vram()

--- main.c
+++ main.c
@@ -20,24 +20,42 @@
 #include "pipe.h"
 #include "pipearray.h"
 #include "patterns.h"
+#include "bird.h"
+
+bird_t bird;
+
+uint16_t CONTROLLER_1_PEDGE;
+uint16_t CONTROLLER_1_PREV;
 
 void fill_vram(void);
 
 // run once on startup
 void reset(void) { 
     pipearray_init();
+    bird_init(&bird);
     load_patterns();  
-    fill_vram();
-    stop();
+    //fill_vram();
+    //stop();
 }
 
 // run 60 times a second
-void do_logic(void) { 
+void do_logic(void) {
+    // calculate positive edge of all controller buttons
+    CONTROLLER_1_PEDGE = (~CONTROLLER_1_PREV) & CONTROLLER_1;
+    CONTROLLER_1_PREV = CONTROLLER_1;
+
     pipearray_move();
+
+    if (CONTROLLER_1_PEDGE & CONTROLLER_A_MASK) bird_flap(&bird);
+
+    bird_move(&bird);
+
+    pipearray_draw();
+    bird_draw(&bird);
 }
 
 // run after do_logic and once gpu is idle
 void fill_vram(void) { 
-    pipearray_draw();
+
 
 }
